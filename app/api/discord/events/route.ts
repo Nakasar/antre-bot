6e918d24bbc069479be12a1ab398e4ab88046ee4
Nakasar <kevin.thizy@outlook.fr>
2025-09-18 19:29:19 +0000

    ✨(discord) Add discord event route

---
+++ app/api/discord/events/route.ts
@@ -0,0 +1,19 @@
+import { isValidRequest } from "discord-verify/node";
+import { NextResponse } from "next/server";
+
+export default async function POST(req: Request) {
+    const body = await req.json();
+    console.debug(body);
+
+    const isValid = await isValidRequest(req, process.env.DISCORD_PUBLIC_KEY ?? '');
+
+    if (!isValid) {
+        return NextResponse.json({ success: false }, { status: 403 });
+    }
+
+    if (body.type === 1) {
+        return NextResponse.json({ type: 1 });
+    }
+
+    return NextResponse.json({ success: true }, { status: 200 });
+}
