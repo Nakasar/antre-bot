7d21396ce5fbe5c8434764029398ab4b9f8108da
Nakasar <kevin.thizy@outlook.fr>
2025-09-18 19:48:30 +0000

    🐛(discord) Fix verify

--- app/api/discord/events/route.ts
+++ app/api/discord/events/route.ts
@@ -1,15 +1,22 @@
-import { isValidRequest } from "discord-verify/node";
+import { verify } from "discord-verify/node";
 import { NextResponse } from "next/server";
+import crypto from 'node:crypto'; 
 
 export async function POST(req: Request) {
     const body = await req.json();
-    console.debug(body);
 
-    const isValid = await isValidRequest(req, process.env.DISCORD_PUBLIC_KEY ?? '');
+    const signature = req.headers.get("x-signature-ed25519");
+	const timestamp = req.headers.get("x-signature-timestamp");
+	const rawBody = JSON.stringify(req.body);
 
+    const isValid = await verify(rawBody, signature, timestamp, process.env.DISCORD_PUBLIC_KEY ?? '', crypto.webcrypto.subtle);
+
+    
     if (!isValid) {
         return NextResponse.json({ success: false }, { status: 403 });
     }
+
+    console.debug(body);
 
     if (body.type === 1) {
         return NextResponse.json({ type: 1 });
